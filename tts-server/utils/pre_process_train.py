c166a0bb5132f1aa35f825e17d2424d1f6f990ef
UPZ <smilish67@naver.com>
2024-12-05 15:39:12 +0900

    speaker, emotion seperating formatter

--- tts-server/utils/pre_process_train.py
+++ tts-server/utils/pre_process_train.py
@@ -2,48 +2,90 @@ import sys
 import os
 import json
 
+
+def audio_formatter(files, pre_path, actor_num, type):
+    for audio_file in files:
+        split = audio_file.split('-')
+        audio_file_name = type+ '_' + split[1]+"_" + actor_num +"_"+ split[4]
+        os.rename(pre_path + '/' + audio_file, f'../dataset/wavs/' + audio_file_name)
+        
+def json_formatter(files, pre_path, actor_num, type):
+    all_text = {}
+    for json_file in files:
+        emotional_type = json_file.split('-')[1][:2]
+        with open(pre_path + '/' + json_file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            if emotional_type not in all_text:
+                all_text[emotional_type] = []
+            all_text[emotional_type].extend(data[0]['script']['normalized'])
+            f.close()
+            
+    for key in all_text:
+        with open(f'../dataset/metadata{type}.csv', 'a', encoding='utf-8') as f:
+            for i in range(len(all_text[key])):
+                # 4 digit number
+                if type == 'T':
+                    f.write(type + '_' + key + '_'+ actor_num + '_'+ f'{i+101:04d}' + '|' + all_text[key][i] + '|' + actor_num + '|' + key +'\n')
+                else:
+                    f.write(type + '_' + key + '_'+ actor_num + '_'+ f'{i+51:04d}' + '|' + all_text[key][i] + '|' + actor_num + '|' + key +'\n')
+            f.close()
+
 if __name__ == "__main__":
-    # 인자를 개별적으로 사용 액터넘버 같이 주시면 됩니다.
-    # ex) python pre_process.py 01
-    if len(sys.argv) > 1:
-        audio_num = sys.argv[1]
+    # origin_dataset 폴더에 모든 성우 폴더를 넣어놓고 실행
+    path = os.path.join(os.path.dirname(__file__), "../origin_dataset")
 
-        json_files_path = f'../dataset/TL_' + audio_num
-        audio_files_path = f'../dataset/TS_' + audio_num
+    train_audio_path = []
+    train_label_path = []
+    val_audio_path = []
+    val_label_path = []
+    
+    #mkdir dataset folder
+    os.makedirs('../dataset/wavs', exist_ok=True)
+    
+    #get all folder start with TL
+    for f in os.listdir(path):
+        if os.path.isdir(os.path.join(path, f)) and f.startswith('TL'):
+            train_label_path.append(f)
+        if os.path.isdir(os.path.join(path, f)) and f.startswith('TS'):
+            train_audio_path.append(f)
+        if os.path.isdir(os.path.join(path, f)) and f.startswith('VL'):
+            val_label_path.append(f)
+        if os.path.isdir(os.path.join(path, f)) and f.startswith('VS'):
+            val_audio_path.append(f)
+            
+    print("train audio 작업 중")
+    for audio_file_path in train_audio_path:
+        actor_num = audio_file_path.split('_')[2]
+        audio_files = os.listdir(os.path.join(path, audio_file_path))
+        audio_formatter(audio_files, "../origin_dataset/"+audio_file_path, actor_num, 'T')
+    
+    print("val audio 작업 중")
+    for audio_file_path in val_audio_path:
+        actor_num = audio_file_path.split('_')[2]
+        audio_files = os.listdir(os.path.join(path, audio_file_path))
+        audio_formatter(audio_files, "../origin_dataset/"+audio_file_path, actor_num, 'V')
+    
+    #initial train metadata.csv
+    with open(f'../dataset/metadataT.csv', 'w', encoding='utf-8') as f:
+        f.write("audio_file|text|speaker_name|emotion_name\n")
+        f.close()
+    
+    print("train label 작업 중")
+    for json_file_path in train_label_path:
+        actor_num = json_file_path.split('_')[2]
+        json_files = os.listdir(os.path.join(path, json_file_path))
+        json_formatter(json_files, "../origin_dataset/"+json_file_path, actor_num, 'T')
+    
+    #initial val metadata.csv
+    with open(f'../dataset/metadataV.csv', 'w', encoding='utf-8') as f:
+        f.write("audio_file|text|speaker_name|emotion_name\n")
+        f.close()
+    
+    print("val label 작업 중")
+    for json_file_path in val_label_path:
+        actor_num = json_file_path.split('_')[2]
+        json_files = os.listdir(os.path.join(path, json_file_path))
+        json_formatter(json_files, "../origin_dataset/"+json_file_path, actor_num, 'V')
 
-        #get all audio files
-        audio_files = os.listdir(audio_files_path)
-        
-        #re name audio files
-        first_idx = 0
-        for i, audio_file in enumerate(audio_files):
-            if i==0:
-                first_idx = int(audio_file.split('.')[0][-4:])
-            audio_file_name = audio_file.split('.')[0][2:4] + audio_file.split('.')[0][-4:] + '.wav'
-            os.makedirs(f'../dataset/actors/{audio_num}/' + audio_file.split('.')[0][2:4]+ '/wavs', exist_ok=True)
-            os.rename(audio_files_path + '/' + audio_file, f'../dataset/actors/{audio_num}' + '/'+ audio_file.split('.')[0][2:4] +'/wavs/' + audio_file_name)
-
-        all_text = {}
-        #iterate each json file
-        for json_file in os.listdir(json_files_path):
-            emotional_type = json_file.split('.')[0][2:4]
-            with open(json_files_path + '/' + json_file, 'r', encoding='utf-8') as f:
-                data = json.load(f)
-                text = data[0]['script']['normalized']
-                text2 = data[0]['script']['origin']
-                for i in range(len(text)):
-                    if emotional_type not in all_text:
-                        all_text[emotional_type] = []
-                    all_text[emotional_type].append((text[i], text2[i]))
-                f.close()
-        
-        #save all text csv
-        for key in all_text:
-            with open(f'../dataset/actors/{audio_num}/{key}/metadata.csv', 'w', encoding='utf-8') as f:
-                for i in range(len(all_text[key])):
-                    # 4 digit number
-                    f.write(key + f'{i+first_idx:04d}' + '|' + all_text[key][i][0] +'|'+ all_text[key][i][1]+ '\n')
-                f.close()
-                    
 
                 
